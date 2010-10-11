4c92c2517435f71c34ce6e70cac5a84b7497a4de
dh-harald <github@criminal.hu>
2010-10-11 17:18:18 +0200

    remove optional

--- GT-I5700/libsensors/Android.mk
+++ GT-I5700/libsensors/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sensors.GT-I5700
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := sensors.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_PRELINK_MODULE := false

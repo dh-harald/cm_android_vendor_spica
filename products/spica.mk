378afdefb643477516a13093e7ef9f5a6679ab4b
dh-harald <github@criminal.hu>
2010-10-15 09:05:24 +0200

    Bluetooth prebuilt and updated Changelog

--- products/spica.mk
+++ products/spica.mk
@@ -146,3 +146,11 @@ PRODUCT_COPY_FILES += \
 #
 PRODUCT_COPY_FILES += \
     vendor/spica/prebuilt/spica/servicemodeapp/serviceModeApp.apk:system/app/serviceModeApp.apk
+
+#
+# Bluetooth
+#
+PRODUCT_COPY_FILES += \
+    vendor/spica/prebuilt/spica/bt/btld:system/bin/btld \
+    vendor/spica/prebuilt/spica/bt/BCM4325D1_004.002.004.0153.0173.hcd:system/bin/BCM4325D1_004.002.004.0153.0173.hcd \
+    vendor/spica/prebuilt/spica/bt/98bthack:system/etc/init.d/98bthack

d71bebe1c5a87387e88cf5e8893552be416b1c57
dh-harald <github@criminal.hu>
2010-11-05 15:10:55 +0100

    OMG, copy/paste typo

--- products/common.mk
+++ products/common.mk
@@ -37,14 +37,14 @@ PRODUCT_PACKAGES += \
     Superuser
 
 # Live wallpaper packages
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
     LiveWallpapers \
     LiveWallpapersPicker \
     MagicSmokeWallpapers \
     librs_jni
 
 # Publish that we support the live wallpaper feature.
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Common CM overlay

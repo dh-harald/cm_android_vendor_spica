b81d2a16e1e31ad55078d538a56ed819cb0bfe1a
dh-harald <github@criminal.hu>
2011-02-17 08:51:19 +0100

    BUMP RC1

--- products/spica.mk
+++ products/spica.mk
@@ -20,6 +20,6 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-SPICA
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7.0.0-RC0-Spica-alpha2
+        ro.modversion=CyanogenMod-7.0.0-RC1-Spica-alpha2
 endif
 
